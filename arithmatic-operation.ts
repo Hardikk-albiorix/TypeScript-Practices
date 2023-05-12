ad3375dad0edbcebb0c630262ed06b99c1a99361
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-05-12 15:02:29 +0530

    feat(arithmatic-operation): implement arithmatic operation example

---
+++ arithmatic-operation.ts
@@ -0,0 +1,18 @@
+const m: number = 20;
+const n: number = 10;
+let z: number;
+
+z = m + n;
+console.log(`Addition of ${m} and ${n} : ${z}`);
+
+z = m - n;
+console.log(`Substraction of ${m} and ${n} : ${z}`);
+
+z = m * n;
+console.log(`Multiplication of ${m} and ${n} : ${z}`);
+
+z = m / n;
+console.log(`Division of ${m} and ${n} : ${z}`);
+
+console.log(`Square of ${m}: `+ m ** 2);
+
